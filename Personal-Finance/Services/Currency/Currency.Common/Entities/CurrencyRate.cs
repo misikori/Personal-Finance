995ddfa589787a456eed5228d33ec04023b5aeca
Stefan <drljevic.stefan00@gmail.com>
2025-08-19 13:01:20 +0200

    feat: define CurrencyRates entities and repository for Currency.Common
    - Added CurrencyRate entity
    - Added CurrencyRateList entity (per-user)
    - Added ICurrencyRatesRepository interface
    - Added CurrencyRatesRepository class
    
    relates to #7

---
+++ Personal-Finance/Services/Currency/Currency.Common/Entities/CurrencyRate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency.Common.Entities
+{
+    internal class CurrencyRate
+    {
+        public string Code { get; set; }
+        public DateTime Date { get; set; }
+        public decimal MiddleRate { get; set; }
+    }
+}
---
+++ Personal-Finance/Services/Currency/Currency.Common/Repositories/ICurrencyRatesRepository.cs
@@ -0,0 +1,17 @@
+﻿using Currency.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency.Common.Repositories
+{
+    internal interface ICurrencyRatesRepository
+    {
+        Task<CurrencyRateList> GetRates(string username);
+        Task<CurrencyRateList> UpdateRates(CurrencyRateList currencyRateList);
+
+        Task DeleteRates(string username);
+    }
+}
