f54824d0067d6c407621e4f20e984e86e0ff46f5
Stefan <drljevic.stefan00@gmail.com>
2025-08-20 09:09:56 +0200

    feat: add extension for service registration in Currency.Common library
    Added AddCurrencyCommonServices extension method to register:
    - Redis cache (using CacheSettings:ConnectionString)
    - CurrencyRatesRepository
    
    Relates to #4

---
+++ Personal-Finance/Services/Currency/Currency.Common/Extensions/CurrencyCommonExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Currency.Common.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency.Common.Extensions
+{
+    public static class CurrencyCommonExtensions
+    {
+        // we will call it in out projects as service.AddCurrencyCommonServices(_configuration);
+        public static void AddCurrencyCommonServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddStackExchangeRedisCache(options => {
+                options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+            });
+            services.AddScoped<ICurrencyRatesRepository,  CurrencyRatesRepository>();
+        }
+    }
+}
