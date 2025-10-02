1c4653a16750f450237f67a5dedcc3c2c2f1935b
Milos Obrenovic <obrenovicmilos01@gmail.com>
2025-10-02 20:53:35 +0200

    chore: summary requests for transactions

--- Personal-Finance/Services/Budget/Budget.Application/Interfaces/ITransactionRepository.cs
+++ Personal-Finance/Services/Budget/Budget.Application/Interfaces/ITransactionRepository.cs
@@ -1,3 +1,4 @@
+using Budget.Application.Queries;
 using Budget.Domain.Entities;
 
 namespace Budget.Application.Interfaces
@@ -5,6 +6,11 @@ namespace Budget.Application.Interfaces
     public interface ITransactionRepository
     {
         Task AddAsync(Transaction transaction);
+
+        Task<IEnumerable<Transaction>> GetForWalletAsync(Guid walletId, DateTime? startDate, DateTime? endDate,
+            string? categoryName);
+
+        Task<IEnumerable<Transaction>> GetExpenseTransactionsMonthAsync(Guid walletId, int month, int year);
         Task SaveChangesAsync();
 
     }
